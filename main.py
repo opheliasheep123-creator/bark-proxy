059051ad2b52f365940eb73d5efd4fb86fd1c095
opheliasheep123-creator <opheliasheep123@gmail.com>
2026-06-27 16:44:16 +0800

    Update main.py

--- main.py
+++ main.py
@@ -76,4 +76,4 @@ def health():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port)from flask import Flask, request, jsonify
+    app.run(host="0.0.0.0", port=port)
